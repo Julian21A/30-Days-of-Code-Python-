cc7183df581d9eddc964a32e69ad4eddc9663cec
Julian21A <Cj.avcj1@gmail.com>
2021-10-19 14:59:20 -0500

    New Update

---
+++ Day 22.py
@@ -0,0 +1,34 @@
+#Day 22: Binary Search Tree
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def getHeight(self,root):
+        heightLeft = 0
+        heightRight = 0
+        if root.left != None: heightLeft = self.getHeight(root.left) + 1
+        if root.right != None: heightRight = self.getHeight(root.right) + 1
+        return max(heightLeft,heightRight)
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+height=myTree.getHeight(root)
+print(height)   
